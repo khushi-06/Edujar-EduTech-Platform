24a7dc57f8efecacee64c4dc43bf1a5ee282e5fe
khushi gupta <guptakhushi622003@gmail.com>
2023-10-27 20:59:41 +0530

    updated the sections

--- src/componets/Client/Client.jsx
+++ src/componets/Client/Client.jsx
@@ -3,7 +3,20 @@ import style from "./Client.Module.css";
 const Client = () => {
   return (
     <div>
-      
+      <div className={`${style.main1} main1`}>
+      <img
+        className={`${style.img9} img9`}
+        src="/images/client-bg.png"
+        alt="image1"
+      />
+
+      <div className={`${style.main2} main2`}>
+<h2>What our client’s say</h2>
+<h4>What our client’s say</h4>
+<h3>What our client’s say</h3>
+      </div>
+      </div>
+
     </div>
   )
 }
