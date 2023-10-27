a15a5c9169c548d4e5b678b31b550bfa98bdddf4
khushi gupta <guptakhushi622003@gmail.com>
2023-10-27 16:49:16 +0530

    added the content

--- src/componets/Categories/Categories.jsx
+++ src/componets/Categories/Categories.jsx
@@ -4,14 +4,18 @@ const Categories = () => {
   return (
     <div>
       <div className={`${style.heading} heading`}>
-            <h4>Explore Top Categories </h4>
-            <h2>Click on the categories and explore all courses</h2>
-        </div>
-        
-    <div className={`${style.content} content`}></div>
-    
-    </div>
-  )
-}
+        <h4>Explore Top Categories </h4>
+        <h2>Click on the categories and explore all courses</h2>
+      </div>
 
-export default Categories
+      <div className={`${style.content} content`}></div>
+      <img
+        className={`${style.img} img`}
+        src="/images/content-bg.png"
+        alt="image1"
+      />
+    </div>
+  );
+};
+
+export default Categories;
